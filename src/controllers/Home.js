352a372ab36deab6ded7bd2422317407a7927004
igor maia <igormaia2001@gmail.com>
2023-02-07 20:46:58 -0500

    database configs

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,6 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({ online: true });
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'igor',
+      sobrenome: 'maia',
+      email: 'igormaia2001@gmail.com',
+      idade: 22,
+      peso: 81,
+      altura: 2.5,
+    });
+    res.json(novoAluno);
   }
 }
 
