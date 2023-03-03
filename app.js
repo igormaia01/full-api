68857df4cc875dbe5e0fd4ccc582e36993f5a891
igormaia01 <igormaia2001@gmail.com>
2023-03-02 22:49:39 -0500

    alunos routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import express from 'express';
 import homeRoutes from './src/routes/home';
 import userRoutes from './src/routes/user';
 import tokenRoutes from './src/routes/token';
+import alunoRoutes from './src/routes/aluno';
 
 class App {
   constructor() {
@@ -24,6 +25,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/user/', userRoutes);
+    this.app.use('/aluno/', alunoRoutes);
     this.app.use('/token/', tokenRoutes);
   }
 }
--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,16 +1,7 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'igor',
-      sobrenome: 'maia',
-      email: 'igormaia2001@gmail.com',
-      idade: 22,
-      peso: 81,
-      altura: 2.5,
-    });
-    res.json(novoAluno);
+    console.log('oi');
+    res.json('ok');
   }
 }
 
