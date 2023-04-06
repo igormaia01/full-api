e8b20afd9ab870712699d5a269b2b1dd5d37fdf0
igormaia01 <igormaia2001@gmail.com>
2023-04-05 21:12:05 -0500

    foto models and migrations

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,6 +4,12 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+      return callback(new multer.MulterError('Arquivo precisa ser JPG ou PNG'));
+    }
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads'));
